06d6c8e0b1bdb14eb7e1b18af57b9f576dd1d549
abikhoir07 <abikhoir42@gmail.com>
2023-05-23 14:52:22 +0700

    create doc scanner

---
+++ app/(tabs)/scanner.tsx
@@ -0,0 +1,96 @@
+import * as MediaLibrary from 'expo-media-library';
+import * as Permissions from 'expo-permissions';
+import React, { useEffect, useState } from 'react';
+import { Alert, Image, PermissionsAndroid, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import DocumentScanner from 'react-native-document-scanner-plugin';
+
+export default () => {
+  const [scannedImage, setScannedImage] = useState();
+  const [nowScan, setNowScan] = useState(1);
+
+  const scanDocument = async () => {
+    if (Platform.OS === 'android' && await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA
+    ) !== PermissionsAndroid.RESULTS.GRANTED) {
+      Alert.alert('Error', 'User must grant camera permissions to use document scanner.')
+      return
+    }
+
+    const { scannedImages } = await DocumentScanner.scanDocument()
+
+    if (scannedImages.length > 0) {
+      setScannedImage(scannedImages[0] as any)
+    }
+  }
+
+  useEffect(() => {
+    // call scanDocument on load
+    scanDocument()
+  }, [nowScan]);
+
+  const handleSave = async () => {
+    // try {
+    //   const granted = await PermissionsAndroid.request(
+    //     PermissionsAndroid.PERMISSIONS.ACCESS_MEDIA_LOCATION,
+    //     {
+    //       title: 'Scanner Camera Permission',
+    //       message:
+    //         'Scanner needs access to your camera ' +
+    //         'so you can take images.',
+    //       buttonNeutral: 'Ask Me Later',
+    //       buttonNegative: 'Cancel',
+    //       buttonPositive: 'OK',
+    //     },
+    //   );
+    //   if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    //     const assert = await MediaLibrary.createAssetAsync(scannedImage);
+    //     MediaLibrary.createAlbumAsync('Arta', assert);
+    //   } else {
+    //     console.log('Camera permission denied');
+    //   }
+    // } catch (err) {
+    //   console.warn(err);
+    // }
+    const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+    if (status === 'granted') {
+      const assert = await MediaLibrary.createAssetAsync(scannedImage);
+      MediaLibrary.createAlbumAsync('Arta', assert);
+    } else {
+      Alert.alert('Error', 'User must grant Media Permission to save scanned image')
+    }
+  }
+
+  const handleScan = () => {
+    setNowScan((prev) => {
+      return prev += 1;
+    })
+  }
+
+  return (scannedImage ? (
+    <View>
+      <Text>Document Scanner</Text>
+      <TouchableOpacity onPress={handleSave} style={styles.btnContainer}>
+        <Text>Click to Download</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleScan} style={styles.btnContainer}>
+        <Text>Scan Again</Text>
+      </TouchableOpacity>
+      <Image
+        resizeMode="contain"
+        style={{ width: '100%', height: '100%' }}
+        source={{ uri: scannedImage }}
+      />
+    </View>) : (<View><Text>Images</Text></View>)
+  )
+}
+
+const styles = StyleSheet.create({
+  btnContainer: {
+    width: '100%',
+    height: 40,
+    backgroundColor: "#FF7754",
+    borderRadius: 12 / 1.25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
